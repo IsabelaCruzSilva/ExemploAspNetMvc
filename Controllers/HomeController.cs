bc629a7e3caf7df62c5ab04c09a35f316ca039c7
Isabela <isabelacruz112@gmail.com>
2022-08-17 11:45:15 -0300

    atualizacoes

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -4,6 +4,12 @@ using ExemploAspNetMvc.Models;
 
 namespace ExemploAspNetMvc.Controllers;
 
+public class UserRequest
+{
+    public string Nome { get; set; }
+    public string Email { get; set; }
+}
+
 public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
@@ -26,6 +32,21 @@ public class HomeController : Controller
     public IActionResult PrimeiraAction()
     {
         return View();
+    }
+
+    public string TesteQueryString([FromQuery]string q, [FromQuery] string nome)
+    {
+        return $"Chegou aqui {q} e {nome}";
+    }
+
+    public string TesteForm([FromForm] UserRequest userRequest)
+    {
+        return $"Nome: {userRequest.Nome}, E-mail: {userRequest.Email}"; 
+    }
+
+    public IActionResult Form()
+    {
+        return View(); 
     }
 
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
